82f1ed64acf2a67850df556cc14f7da8784935a3
edwin <edwin@homerlogistics.com>
2016-01-23 22:20:58 -0500

    fixed server.js to allow PORT to be passed in as arg

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var express = require('express');
 var nameGenerator = require('./cumberbatched_names.js');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   var jsonPayload = {
     response_type: req.query["response_type"],
@@ -16,7 +18,6 @@ app.get('/', function (req, res) {
   res.send(jsonPayload);
 });
 
-
-app.listen(3000, function () {
-  console.log("Now serving Cumberbatch'ed Names on port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
